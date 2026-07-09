a49920d92d5579f2e917175ae67ec6caacdda9ad
surajjha0622 <jhasuraj1956@gmail.com>
2026-07-09 08:17:39 +0530

    Add scroll progress functionality to update progress bar on page scroll

--- js/main.js
+++ js/main.js
@@ -33,3 +33,19 @@ document.querySelectorAll(".btn").forEach(btn=>{
     });
 
 });
+
+// =========================
+// SCROLL PROGRESS
+// =========================
+
+window.addEventListener("scroll",()=>{
+
+const scrollTop=document.documentElement.scrollTop;
+
+const height=document.documentElement.scrollHeight-document.documentElement.clientHeight;
+
+const progress=(scrollTop/height)*100;
+
+document.getElementById("progress-bar").style.width=progress+"%";
+
+});
